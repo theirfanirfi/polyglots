a31654d51deada9e3dc0ac2de2f6f5de511243b6
= <>
2021-02-07 18:05:32 +0500

    updation completed

--- application/views/UpdateLessonView.py
+++ application/views/UpdateLessonView.py
@@ -1,9 +1,10 @@
 from flask_classful import FlaskView, route
-from flask import request, flash, redirect, url_for, render_template
+from flask import request, flash, redirect, url_for, render_template, jsonify
 from application import app, db
 from application.utils import process_lesson, save_file
 from application.models.models import *
 from application.forms.forms import LessonForm
+import json
 
 
 class UpdateLessonView(FlaskView):
@@ -48,19 +49,109 @@ class UpdateLessonView(FlaskView):
 
     @route('/images_lesson/<int:id>', methods=['GET', 'POST'])
     def images_lesson(self, id):
+        form = LessonForm()
+        lesson = TapWhatYouHear.query.get_or_404(id)
+        group = Groups.query.get_or_404(lesson.group_id)
+        images = json.loads(lesson.images)
+        images_words = json.loads(lesson.words_for_images)
+        print(images_words)
+
         if request.method == "GET":
-            pass
+            return render_template("update_lessons/four_images_lesson.html",
+                                   group=group,
+                                   form=form,
+                                   lesson=lesson, images=images, words=images_words)
         elif request.method == "POST":
-            pass
+            areWordsInserted, howManyInserted, totalWords = process_lesson(request, group)
+
+            files = request.files.getlist('image[]')
+            bottom_words = request.form.getlist('bottom_word[]')
+            correct_option = request.form['correct_option']
+            sentence = request.form['sentence']
+            sentence_type = request.form['sentence_type']
+
+            if correct_option == None or correct_option == "" or (sentence == None or sentence == ""):
+                print('correct option')
+                flash('Correct option must be provided', 'danger')
+                return redirect(request.referrer)
+
+            images_list = list()
+            words_list = list()
+            print(len(files))
+            if files:
+                for file in files:
+                    if file:
+                        isUploaded, file_name = save_file(file, 'lesson')
+                        if not isUploaded:
+                            flash('Error occurred in uploading the image, please try again.', 'danger')
+                            print('file not uploaded')
+                            return redirect(request.referrer)
+                        images_list.append(file_name)
+                lesson.images = str(json.dumps(images_list))
+
+            if bottom_words:
+                for word in bottom_words:
+                    words_list.append(word)
+                lesson.words_for_images = str(json.dumps(words_list))
+
+            lesson.translation = correct_option
+            lesson.sentence = sentence
+            lesson.masculine_feminine_neutral = sentence_type
+            try:
+                db.session.add(lesson)
+                db.session.commit()
+                print('committed')
+                flash('Lesson Update', 'info')
+                return redirect(request.referrer)
+            except Exception as e:
+                print(e)
+                flash("Error occurred in updating the lesson, please try again.", "danger")
+                return redirect(request.referrer)
         else:
             return 'invalid request'
 
     @route('/single_image_lesson/<int:id>', methods=['GET', 'POST'])
     def single_image_lesson(self, id):
+        form = LessonForm()
+        lesson = SentenceLesson.query.get_or_404(id)
+        group = Groups.query.get_or_404(lesson.group_id)
         if request.method == "GET":
-            pass
+            # request.form['sentence'] = "wow"
+            return render_template("update_lessons/single_image_lesson.html",
+                                   group=group,
+                                   form=form,
+                                   lesson=lesson)
         elif request.method == "POST":
-            pass
+            # areWordsInserted, howManyInserted, totalWords = process_lesson(request, group)
+            file = request.files['image']
+            correct_option = request.form['correct_sentence_word']
+            sentence = request.form['sentence']
+            sentence_type = request.form['sentence_type']
+            if correct_option == None or correct_option == "" or (sentence == None or sentence == ""):
+                flash('Correct option must be provided', 'danger')
+                return redirect(request.referrer)
+
+            if file:
+                isSaved, file_name = save_file(file, 'lesson')
+                if not isSaved:
+                    flash('Error occurred. Please try again.', 'danger')
+                    return redirect(request.referrer)
+                lesson.images = file_name
+
+            lesson.translation = correct_option
+            lesson.sentence = sentence
+            lesson.is_type_answer = 1 if 'answer_to_type' in request.form else 0
+            lesson.masculine_feminine_neutral = sentence_type
+            try:
+                db.session.add(lesson)
+                db.session.commit()
+                print('committed')
+                flash('Lesson Updated', 'info')
+                return redirect(request.referrer)
+            except Exception as e:
+                print(e)
+                flash("Error occurred in updating the lesson, please try again.", "danger")
+                return redirect(request.referrer)
         else:
             return 'invalid request'
 
@@ -126,18 +217,83 @@ class UpdateLessonView(FlaskView):
 
     @route('/pairs_to_match/<int:id>', methods=['GET', 'POST'])
     def pairs_to_match(self, id):
+        form = LessonForm()
+        lesson = PairsToMatch.query.get_or_404(id)
+        group = Groups.query.get_or_404(lesson.group_id)
         if request.method == "GET":
-            pass
+            return render_template("update_lessons/pairs_to_match.html",
+                                   group=group,
+                                   form=form,
+                                   lesson=lesson)
         elif request.method == "POST":
-            pass
+            areWordsInserted, howManyInserted, totalWords = process_lesson(request, group)
+            sentence = request.form['sentence']
+            if sentence == None or sentence == "":
+                print('Error: validation ')
+                flash('Sentence must be entered.', 'danger')
+                return redirect(request.referrer)
+
+            if request.files['sound']:
+                isSaved, file_name = save_file(request.files['sound'], 'lesson')
+                if isSaved:
+                    lesson.sounds = file_name
+
+            lesson.group_id = group.group_id
+            lesson.language_id = group.language_id
+            lesson.sentence = sentence
+
+            try:
+                db.session.add(lesson)
+                db.session.commit()
+                flash('Lesson Updated.', 'info')
+                return redirect(request.referrer)
+            except Exception as e:
+                print(e)
+                flash('Error occurred in updating the lesson. Please try again.', 'danger')
+                return redirect(request.referrer)
         else:
             return 'invalid request'
 
     @route('/tap_what_you_hear/<int:id>', methods=['GET', 'POST'])
     def tap_what_you_hear(self, id):
+        form = LessonForm()
+        lesson = TapWhatYouHear.query.get_or_404(id)
+        group = Groups.query.get_or_404(lesson.group_id)
         if request.method == "GET":
-            pass
+            return render_template("update_lessons/tap_what_you_hear.html",
+                                   group=group,
+                                   form=form,
+                                   lesson=lesson)
         elif request.method == "POST":
-            pass
+            correct_option = request.form['correct_option']
+            tags = request.form['tags']
+
+            if correct_option == None or correct_option == "":
+                print('Error: validation ')
+                flash('correct option must be entered.', 'danger')
+                return redirect(request.referrer)
+
+
+            if request.files['sound']:
+                isSaved, file_name = save_file(request.files['sound'], 'lesson')
+                if not isSaved:
+                    flash('Sound not uploaded. Please try again.', 'danger')
+                    return redirect(request.referrer)
+                lesson.sounds = file_name
+
+            lesson.group_id = group.group_id
+            lesson.language_id = group.language_id
+            lesson.options_tags = tags
+            lesson.translation = correct_option
+
+            try:
+                db.session.add(lesson)
+                db.session.commit()
+                flash('Lesson Update.', 'info')
+                return redirect(request.referrer)
+            except Exception as e:
+                print(e)
+                flash('Error occurred in updating the lesson. Please try again.', 'danger')
+                return redirect(request.referrer)
         else:
             return 'invalid request'
